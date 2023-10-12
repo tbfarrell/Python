bfd6f695868a58481cad317725807b6fb23db85d
tbfarrell <122227515+tbfarrell@users.noreply.github.com>
2023-10-11 17:27:31 -0700

    Create FAHControl.bash
    Homemade bash script for FAHControl desktop shortcut

---
+++ FAHControl.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Name[en_US]=FAHControl
+Exec=/home/general/FAHControl.sh
+Icon=/home/general/Downloads/fahcontrol.png
+Icon[en_US]=/home/general/Downloads/fahcontrol.png
+Name=FAHControl
